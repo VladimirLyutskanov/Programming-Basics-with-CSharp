7a6812dc82a84134b5e002a59ff012c73356580c
Vlad <vollos@abv.bg>
2019-07-04 16:17:16 +0200

    four tasks

---
+++ First Steps In Coding/04. Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
