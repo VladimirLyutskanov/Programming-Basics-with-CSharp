cb42cacb6463111691c986372305846f27f561fe
Vlad <vollos@abv.bg>
2019-07-08 09:39:11 +0200

    simple calc added

---
+++ Simple Operations and Calculations - Lab/05. Projects Creation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The architect {name} will need {projects * 3} hours to complete {projects} project/s.");
+        }
+    }
+}
---
+++ Simple Operations and Calculations - Lab/07. Pet Shop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = a * b * c;
+            double litres = volume * 0.001;
+            double realPercent = percent * 0.01;
+            double realVolume = litres * (1 - realPercent);
+
+            Console.WriteLine($"{realVolume:F3}");
+        }
+    }
+}
