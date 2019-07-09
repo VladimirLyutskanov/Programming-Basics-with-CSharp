5c8b53079d6bc486cd6496dd1370804a1811f0a9
Vlad <vollos@abv.bg>
2019-07-09 08:32:54 +0200

    exercise

---
+++ Simple Operations and Calculations - Exercise/01. USD към BGN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USD_към_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double lev = usd * 1.79549;
+            Console.WriteLine(Math.Round(lev, 2));
+        }
+    }
+}
---
+++ Simple Operations and Calculations - Exercise/04. Tailoring Workshop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Tailoring_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tables = double.Parse(Console.ReadLine());
+            double tablesLenght = double.Parse(Console.ReadLine());
+            double tablesWidth = double.Parse(Console.ReadLine());
+            double tabelearea = tables * (tablesLenght + 2 * 0.3) * (tablesWidth + 2 * 0.3);
+            double squarearea = tables * (tablesLenght / 2) * (tablesLenght / 2);
+            double priceUsd = tabelearea * 7 + squarearea * 9;
+            double priceLeva = priceUsd * 1.85;
+
+            Console.WriteLine($"{priceUsd:F2} USD");
+            Console.WriteLine($"{priceLeva:F2} BGN");
+        }
+    }
+}
