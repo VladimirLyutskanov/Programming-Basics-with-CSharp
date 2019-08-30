36450a1265704abb6b1db9c571cf634a52e8a90a
Vlado <vollos@abv.bg>
2019-08-30 09:44:06 +0300

    for cicle

---
+++ forCicle Lab/forCicle Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forCicle_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
